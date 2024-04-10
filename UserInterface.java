15f5827dad309a46f8bba968bbfae0175d7566dc
Danielprakash23 <146421091+Danielprakash23@users.noreply.github.com>
2024-04-10 20:32:09 +0530

    Add files via upload

---
+++ UserInterface.java
@@ -0,0 +1,54 @@
+package com.FindPetName;
+
+
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.io.IOException;
+	import java.util.Scanner;
+	import java.util.regex.Matcher;
+	import java.util.regex.Pattern;
+
+	public class UserInterface{
+
+
+	    public static void main(String[] args) {
+	        String filename="C:\\Users\\RIJO\\eclipse-workspace\\CoreJava\\src\\com\\FindPetName\\names.txt";
+	        findPetName(filename);
+	    }
+	    public static void findPetName(String filename){
+	    try {
+
+	        File f1 = new File(filename);
+	        if (!f1.exists()){
+	            throw new Exception("Please give the correct fi le name");
+	        }
+
+	        Scanner names = new Scanner(f1);
+	        while (names.hasNextLine()) {
+	            String petName = names.nextLine();
+	            Pattern regx = Pattern.compile("[aeiouAEIOU]");
+	            Matcher matcher = regx.matcher(petName);
+	            int count = 0;
+	            Boolean finded = false;
+	            while (matcher.find()) {
+	                count++;
+	                if (count > 1) {
+	                    System.out.println(petName);
+	                    finded = true;
+	                    break;
+	                }
+	            }
+	            if (finded == true) {
+	                break;
+	            }
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Error occurred while reading the names from file");
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+
+
+	    }
+	}
+
